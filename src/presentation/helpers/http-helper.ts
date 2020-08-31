05d3b17264fccb4fbfafca5191c06a2c55cee439
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-08-31 18:42:48 -0300

    refactor: move duplicated code to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { HttpResponse } from '../protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
