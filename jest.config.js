1a4d5fc02fe36b6cfdc436959e3ea160a2ae50cc
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-01 17:59:49 -0300

    fix: fix bug coverage jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
     '!**/protocols/**'
   ],
   coverageDirectory: 'coverage',
